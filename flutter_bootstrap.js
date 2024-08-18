b63d54518d26633ab049b9a42305570e1d5b616d
ColinSchmale <cs@nuuk.de>
2024-08-18 22:52:00 +0200

    wip

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"235db911ba279722f5e685f38b0ed30fa7e857
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2006401178"
+    serviceWorkerVersion: "688091407"
   }
 });
