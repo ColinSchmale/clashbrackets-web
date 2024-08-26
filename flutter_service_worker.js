f039ee9684465ea22d328392dd041f4767a611c3
ColinSchmale <cs@nuuk.de>
2024-08-27 01:24:38 +0200

    wip

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,14 +3,14 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "94b5e2c34653313d44f2ada01434642d",
+const RESOURCES = {"flutter_bootstrap.js": "cf1e2e1ea6a1828e965e3dc8e32b6d73",
 "version.json": "185562450ee55176c6a6451671686e5b",
-"index.html": "6d0485861454bfe8506feb810e5d0c7b",
-"/": "6d0485861454bfe8506feb810e5d0c7b",
+"index.html": "fb113ee6b64808b1dac4d0c525686deb",
+"/": "fb113ee6b64808b1dac4d0c525686deb",
 "privacy-policy.html": "747290d3f6f3c49944805cc03f19d8f6",
 "contact.html": "968f7fb23529f1167b67e0a8b8a25f1f",
 "CNAME": "07b80f1450f2e31ab189eb1c764937ae",
-"main.dart.js": "08d1ae1d0e7293e9694a6f508aa20f1c",
+"main.dart.js": "f8fa619ab04766d3baea1752b3eb9ff9",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
 "README.md": "4337d03d259a6e2fe8d319b5ae16aba1",
 "favicon.png": "e719b63ace400c76f660c3317c48a31a",
